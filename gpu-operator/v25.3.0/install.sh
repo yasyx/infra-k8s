bb7409a65613b6fbcbc660143a69995a4cad38d1
cuisongliu <cuisongliu@qq.com>
2025-05-17 14:26:58 +0800

    add images

--- gpu-operator/v25.3.0/install.sh
+++ gpu-operator/v25.3.0/install.sh
@@ -4,6 +4,6 @@ set -e
 NAME=${NAME:-"gpu-operator"}
 NAMESPACE=${NAMESPACE:-"gpu-operator"}
 CHARTS=${CHARTS:-"./charts/gpu-operator"}
-HELM_OPTS=${HELM_OPTS:-"--set driver.enabled=false --set toolkit.enabled=false"}
+HELM_OPTS=${HELM_OPTS:-""}
 
 helm upgrade --wait -i ${NAME} ${CHARTS} -n ${NAMESPACE} --create-namespace -f values.yaml ${HELM_OPTS}
