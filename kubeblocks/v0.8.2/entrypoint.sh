3453351a5aa425c7cace5f4d9871bdd63b088a43
cuisongliu <cuisongliu@qq.com>
2025-05-19 00:30:09 +0800

    fix minio

--- kubeblocks/v0.8.2/entrypoint.sh
+++ kubeblocks/v0.8.2/entrypoint.sh
@@ -7,7 +7,6 @@ sleep 2
 helm upgrade -i kubeblocks charts/kubeblocks --set snapshot-controller.enabled=false --insecure-skip-tls-verify -n kb-system --create-namespace
 cp -rf opt/kbcli /usr/bin/
 kbcli addon enable apecloud-mysql
-kbcli addon enable csi-s3
 kbcli addon enable redis
 kbcli addon enable postgresql
 kbcli addon enable mongodb
