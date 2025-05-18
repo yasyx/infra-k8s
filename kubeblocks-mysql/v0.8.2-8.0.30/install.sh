60a3e6d6a4af8dcd7aa8a3d3b5bc5b8efdc00b49
cuisongliu <cuisongliu@qq.com>
2025-05-18 14:28:12 +0800

    add images

--- kubeblocks-mysql/v0.8.2-8.0.30/install.sh
+++ kubeblocks-mysql/v0.8.2-8.0.30/install.sh
@@ -9,3 +9,14 @@ HELM_OPTS=${HELM_OPTS:-""}
 helm upgrade -i ${NAME} ${CHARTS} -n ${NAMESPACE} --create-namespace ${HELM_OPTS}
 helm delete -n grafana-operator ${NAME} || true
 helm upgrade -i ${NAME} ./charts/mysql-grafana -n grafana-operator
+
+kbcli backuprepo create backuprepo-mysql \
+  --provider minio \
+  --endpoint minio.minio.svc:9000 \
+  --bucket backuprepo \
+  --access-key-id admin \
+  --secret-access-key admin123 \
+  --access-method Tool \
+  --default
+
+kbcli backuprepo list
