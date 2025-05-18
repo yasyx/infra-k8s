3ac8f3f767064d9848bb72c0db433204fe6a2d9c
cuisongliu <cuisongliu@qq.com>
2025-05-18 14:21:07 +0800

    add images

--- data-infra-pg/v0.7.2-14.8.0/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-kbcli addon enable postgresql
-NAME=${NAME:-"postgresql"}
-NAMESPACE=${NAMESPACE:-"postgresql"}
-CHARTS=${CHARTS:-"./charts/postgresql"}
-HELM_OPTS=${HELM_OPTS:-""}
-
-helm upgrade -i ${NAME} ${CHARTS} -n ${NAMESPACE} --create-namespace ${HELM_OPTS}
--- kubeblocks/v0.8.2/entrypoint.sh
+++ kubeblocks/v0.8.2/entrypoint.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
-kubectl apply -f crds/
+#!/usr/bin/env bash
+
+set -e
+
+kubectl create -f charts/kubeblocks/crds/kubeblocks_crds.yaml || kubectl replace -f charts/kubeblocks/crds/kubeblocks_crds.yaml
+sleep 2
 helm upgrade -i kubeblocks charts/kubeblocks --set snapshot-controller.enabled=false --insecure-skip-tls-verify -n kb-system --create-namespace
 cp -rf opt/kbcli /usr/bin/
-KB_OPTS=${KB_OPTS:-"--set kubeletPath=/var/lib/kubelet"}
 kbcli addon enable apecloud-mysql
-kbcli addon enable mysql
-#https://github.com/apecloud/helm-charts/releases/download/csi-s3-0.31.4/csi-s3-0.31.4.tgz
-kbcli addon enable csi-s3  ${KB_OPTS}
+kbcli addon enable csi-s3
 kbcli addon enable redis
 kbcli addon enable postgresql
+kbcli addon enable mongodb
