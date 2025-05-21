d91c5c0371a9848f92ab7417c68d0090d1cc64d2
cuisongliu <cuisongliu@qq.com>
2025-05-21 14:30:52 +0800

    add treafik

---
+++ treafik/v3.1.0/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -e
+cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
+HELM_OPTS=${HELM_OPTS:-""}
+helm upgrade --install traefik charts/traefik -n traefik --create-namespace ${TRAEFIK_HELM_OPTS}
